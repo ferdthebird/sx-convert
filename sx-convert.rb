11c8a499192dbd2f471e55911e7190d1e8459a76
Tim <tim.edwards@nuku.com>
2013-07-04 11:03:48 -0700

    modified lic

--- sx-convert.rb
+++ sx-convert.rb
@@ -4,10 +4,23 @@ require "date"
 #
 # a ruby program that converts shoutcast log files to the new soundexchange required reporting format.
 # reads and writes to stdin/stdout by default.  use -i or -o flags to set an input and/or output file
-# use -s to specify a stream_id.  License is GPLv3 - see enclosed LICENSE file for details
+# use -s to specify a stream_id. 
 #
 # tim@lifelike.com
 #
+# Copyright 2013 Tim M Edwards. All rights reserved.
+# 
+# Redistribution and use, with or without modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
 
 class LogEntry
   attr_accessor :uid, :ip_address, :date, :duration, :status, :agent, :stream_id
